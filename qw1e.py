ab409eccb7c4c44e01f2787eb07f514c3d0d7512
policememos <policememos@gmail.com>
2022-04-14 10:04:37 +0500

    генератор простых чисел

---
+++ qw1e.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+def is_prime(n):
+    if (n <= 1):
+        return False
+    if (n == 2):
+        return True
+    if (n % 2 == 0):
+        return False
+
+    i = 3
+    while i <= sqrt(n):
+        if n % i == 0:
+            return False
+        i = i + 2
+
+    return True
+
+
+def prime_generator():
+    n = 1
+    while True:
+        n += 1
+        if is_prime(n):
+            yield n
+            
+gen = prime_generator()   
+res = []
+for i in range(20):
+    
+    res.append(next(gen))
+print(*res)
